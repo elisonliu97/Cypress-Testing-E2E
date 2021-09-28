7dd4e3cd7f49195f44b3d4d228df44a7fbac5fbd
Elison <elisonliu97@gmail.com>
2021-09-28 19:18:46 -0400

    tests to create and login accounts

---
+++ cypress/integration/page-object-models/LoginPage.js
@@ -0,0 +1,78 @@
+/// <reference types="cypress" />
+
+export class LoginPortal {
+
+    loginPagePortalEl = '.login';
+    signupEmailInputEl = '#email_create';
+    submitCreateEl = '#SubmitCreate';
+    firstNameEl = '#customer_firstname'
+    lastNameEl = '#customer_lastname'
+    passwordEl = '#passwd'
+    addressEl = '#address1'
+    cityEl = '#city'
+    stateEl = '#id_state'
+    postCodeEl = '#postcode'
+    phoneEl = '#phone_mobile'
+    submitAccountEl = '#submitAccount'
+
+    loginEmailEl = '#email'
+    loginSubmitEl = '#SubmitLogin'
+
+    visitLoginPage() {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get(this.loginPagePortalEl).click();
+    }
+
+    enterEmailCreate(email) {
+        cy.get(this.signupEmailInputEl).type(email);
+        cy.get(this.submitCreateEl).click();
+    }
+
+    enterFirstName(firstName) {
+        cy.get(this.firstNameEl).type(firstName);
+    }
+    
+    enterLastName(lastName) {
+        cy.get(this.lastNameEl).type(lastName);
+    }
+
+    enterPassword(password) {
+        cy.get(this.passwordEl).type(password);
+    }
+
+    enterAddress(address) {
+        cy.get(this.addressEl).type(address);
+    }
+
+    enterCity(city) {
+        cy.get(this.cityEl).type(city);
+    }
+
+    enterState(state) {
+        cy.get(this.stateEl).select(state);
+    }
+
+    enterPostCode(postCode) {
+        cy.get(this.postCodeEl).type(postCode);
+    }
+
+    enterPhoneNumber(phoneNumber) {
+        cy.get(this.phoneEl).type(phoneNumber);
+    }
+
+    submitAccount() {
+        cy.get(this.submitAccountEl).click();
+    }
+
+    enterEmailLogin(email) {
+        cy.get(this.loginEmailEl).type(email);
+    }
+
+    enterPasswordLogin(password) {
+        cy.get(this.passwordEl).type(password);
+    }
+
+    submitLogin() {
+        cy.get(this.loginSubmitEl).click();
+    }
+}
--- cypress/integration/tests/test.js
+++ cypress/integration/tests/test.js
@@ -1,6 +1,42 @@
 /// <reference types="cypress" />
+import { LoginPortal } from '../page-object-models/LoginPage';
 
-// it('goes to login page', () => {
-//     cy.visit('http://automationpractice.com/index.php')
-//     cy.get('.login').click()
-// })
+describe('Create account page tests', () => {
+    const loginPortal = new LoginPortal();
+    const userData = [
+        {
+            email: 'testuser2@email.com',
+            firstName: 'testtwo',
+            lastName: 'user',
+            password: 'passtesttwo',
+            address: '2 test drive',
+            city: 'Test City',
+            state: 'Iowa',
+            postCode: '10101',
+            phoneNumber: '1234567890'
+        }
+    ]
+
+    beforeEach(() => {
+        loginPortal.visitLoginPage();
+    }) 
+
+    it('should be able to create an account given proper credentials', () => {
+        loginPortal.enterEmailCreate(userData[0].email);
+        loginPortal.enterFirstName(userData[0].firstName);
+        loginPortal.enterLastName(userData[0].lastName);
+        loginPortal.enterPassword(userData[0].password);
+        loginPortal.enterAddress(userData[0].address);
+        loginPortal.enterCity(userData[0].city);
+        loginPortal.enterState(userData[0].state);
+        loginPortal.enterPostCode(userData[0].postCode);
+        loginPortal.enterPhoneNumber(userData[0].phoneNumber);
+        loginPortal.submitAccount();
+    })
+
+    it('should be able to login on a created account given proper credentials', () => {
+        loginPortal.enterEmailLogin(userData[0].email);
+        loginPortal.enterPasswordLogin(userData[0].password);
+        loginPortal.submitLogin();
+    })
+})
