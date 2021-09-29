e4e312a0ca3c4c0b6b00a95416bfa6207a93af76
Elison <elisonliu97@gmail.com>
2021-09-29 00:54:41 -0400

    added additional properties in accoutn creation

--- cypress/integration/page-object-models/LoginPage.js
+++ cypress/integration/page-object-models/LoginPage.js
@@ -16,6 +16,19 @@ export class LoginPortal {
     stateEl = '#id_state';
     postCodeEl = '#postcode';
     phoneEl = '#phone_mobile';
+    titleMEl = '#id_gender1';
+    titleFEl = '#id_gender2';
+    dayEl = '#days';
+    monthEl = '#months';
+    yearEl = '#years';
+    optinEl = '#optin';
+    companyEl = '#company';
+    address2El = '#address2';
+    countryEl = '#id_country';
+    otherEl = '#other';
+    homephoneEl = '#phone';
+    aliasEl = '#alias';
+    newsletterEl = '#newsletter';
     submitAccountEl = '#submitAccount';
     createAccountErrorEl = '#create_account_error';
 
@@ -76,6 +89,52 @@ export class LoginPortal {
         cy.get(this.phoneEl).type(phoneNumber);
     }
 
+    enterTitle(title) {
+        if (title === 'Mr.') {
+            cy.get(this.titleMEl).click();
+        } else if (title === "Mrs.") {
+            cy.get(this.titleFEl).click();
+        }
+    }
+
+    enterDoB(dob) {
+        cy.get(this.dayEl).select(dob.day);
+        cy.get(this.monthEl).select(dob.month);
+        cy.get(this.yearEl).select(dob.year);
+    }
+
+    checkNewsletter(check) {
+        if (check) {
+            cy.get(this.newsletterEl).click();
+        }
+    }
+
+    checkOptin(opt) {
+        if (opt) {
+            cy.get(this.optinEl).click();
+        }
+    }
+
+    enterCompany(company) {
+        cy.get(this.companyEl).type(company);
+    }
+
+    enterAddress2(address2) {
+        cy.get(this.address2El).type(address2);
+    }
+
+    enterOther(other) {
+        cy.get(this.otherEl).type(other);
+    }
+
+    enterHomePhone(homePhone) {
+        cy.get(this.homephoneEl).type(homePhone);
+    }
+
+    enterAlias(alias) {
+        cy.get(this.aliasEl).type(alias);
+    }
+
     submitAccount() {
         cy.get(this.submitAccountEl).click();
     }
@@ -87,6 +146,7 @@ export class LoginPortal {
     // test function to create new account
     fillCreateAccDetails(userDataFixture) {
         cy.fixture(userDataFixture).then((data) => {
+            // essential
             this.enterEmailCreate(data.email);
             this.enterFirstName(data.firstName);
             this.enterLastName(data.lastName);
@@ -96,6 +156,37 @@ export class LoginPortal {
             this.enterState(data.state);
             this.enterPostCode(data.postCode);
             this.enterPhoneNumber(data.phoneNumber);
+
+            // extra
+            if (data.title) {
+                this.enterTitle(data.title);
+            }
+            if (data.dob) {
+                this.enterDoB(data.dob);
+            }
+            if (data.newsletter) {
+                this.checkNewsletter(data.newsletter);
+            }
+            if (data.optin) {
+                this.checkOptin(data.optin);
+            }
+            if (data.company) {
+                this.enterCompany(data.company);
+            }
+            if (data.address2) {
+                this.enterAddress2(data.address2);
+            }
+            if (data.other) {
+                this.enterOther(data.other);
+            }
+            if (data.homePhone) {
+                this.enterHomePhone(data.homePhone);
+            }
+            if (data.alias) {
+                this.enterAlias(data.alias);
+            }
+
+            // submit
             this.submitAccount();
         })
     }
--- cypress/integration/tests/account-creation-tests.js
+++ cypress/integration/tests/account-creation-tests.js
@@ -13,6 +13,16 @@ describe('Create account page tests', () => {
         loginPortal.getLogoutBtn().should('be.visible');
     })
 
+    it('should be able to create another account given proper credentials', () => {
+        loginPortal.fillCreateAccDetails('user2');
+        loginPortal.getLogoutBtn().should('be.visible');
+    })
+
+    it('should be able to create another account given differing credentials', () => {
+        loginPortal.fillCreateAccDetails('user3');
+        loginPortal.getLogoutBtn().should('be.visible');
+    })
+
     it ('should not be able to create an account using credentials that have already been used', () => {
         loginPortal.useExistingAccDetails('user1');
         loginPortal.getCreateAccountError().should('be.visible');
