5f1cee08407089bb827acf4643db6646144303ae
Elison Liu <elisonliu97@gmail.com>
2021-09-29 13:48:13 -0400

    bdd comments for each test

--- cypress/integration/tests/account-creation-tests.js
+++ cypress/integration/tests/account-creation-tests.js
@@ -8,21 +8,33 @@ describe('Create account page tests', () => {
         loginPortal.visitLoginPage();
     }) 
 
+    // GIVEN proper credentials with only the bare minimum information necessary
+    // WHEN a user signs up for an account
+    // THEN it should create an account and log the user in
     it('should be able to create an account given proper credentials', () => {
         loginPortal.fillCreateAccDetails('user1');
         loginPortal.getLogoutBtn().should('be.visible');
     })
 
+    // GIVEN proper credentials with some extra information
+    // WHEN a user signs up for an account
+    // THEN it should create an account and log the user in
     it('should be able to create another account given proper credentials', () => {
         loginPortal.fillCreateAccDetails('user2');
         loginPortal.getLogoutBtn().should('be.visible');
     })
 
+    // GIVEN proper credentials with full information for signup
+    // WHEN a user signs up for an account
+    // THEN it should create an account and log the user in
     it('should be able to create another account given differing credentials', () => {
         loginPortal.fillCreateAccDetails('user3');
         loginPortal.getLogoutBtn().should('be.visible');
     })
 
+    // GIVEN credentials that are already registered
+    // WHEN a user signs up for an account
+    // THEN it should give an error saying that an account already exists
     it ('should not be able to create an account using credentials that have already been used', () => {
         loginPortal.useExistingAccDetails('user1');
         loginPortal.getCreateAccountError().should('be.visible');
--- cypress/integration/tests/account-verification-tests.js
+++ cypress/integration/tests/account-verification-tests.js
@@ -4,16 +4,25 @@ import { LoginPortal } from '../page-object-models/LoginPage';
 describe('Tests for created accounts', () => {
     const loginPortal = new LoginPortal();
 
+    // GIVEN proper credentials to an already existing account
+    // WHEN the user logs in
+    // THEN it logs the user in and the logout button should be visible
     it('should be able to login on a created account given proper credentials', () => {
         loginPortal.fillLoginCreds('user1');
         loginPortal.getLogoutBtn().should('be.visible');
     })
 
+    // GIVEN false credentials
+    // WHEN the user tries to log in
+    // THEN it should display an error message and the login button should still be visible
     it('should not be able to login when given wrong credentials', () => {
         loginPortal.fillLoginCreds('false-user');
         loginPortal.getLoginBtn().should('be.visible');
     })
 
+    // GIVEN a logged in account
+    // WHEN the user tries to complete an order checkout
+    // THEN the user should get to a page that confirms the order
     it('should be able to complete a checkout when logged in', () => {
         loginPortal.fillLoginCreds('user1');
         loginPortal.goToProductSection();
